f91da6f28620600b2e4caec5b429a9f7c6d896cd
walterhuang <isponder@gmail.com>
2016-10-20 23:27:52 -0700

    add Contact and IContactRepository

---
+++ DapperDiaper/IContactRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDiaper
+{
+    public interface IContactRepository
+    {
+        Contact Find(int id);
+        List<Contact> GetAll();
+        Contact Add(Contact contact);
+        Contact Update(Contact contact);
+        void Remove(int id);
+
+        Contact GetFullContact(int id);
+        void Save(Contact contact);
+    }
+}
